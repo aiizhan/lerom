17b60e0466d6feb610dedc8b58beb2f5a3b47a31
perizatdev <perizatkalykova2@gmail.com>
2024-04-22 13:50:13 +0300

    rooms

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Layout from './components/Layout/Layout'
 import HomePage from './Pages/HomePage'
+import Odalar from "./components/odalar/Odalar.jsx";
 
 
 export default function App() {
@@ -10,7 +11,8 @@ export default function App() {
    <div className='App'>
     <Routes>
       <Route path='/' element={<Layout/>} >
-      <Route  main element={<HomePage/>}  />      
+      <Route  main element={<HomePage/>}  />
+        <Route path="/" element={<Odalar/>}/>
       
 
       </Route>
